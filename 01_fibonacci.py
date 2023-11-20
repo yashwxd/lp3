fc3a994c547d5dc0582be42299730a434fb92ef5
yashwxd <113768247+yashwxd@users.noreply.github.com>
2023-11-21 03:02:56 +0530

    01_fibonacci.py

---
+++ 01_fibonacci.py
@@ -0,0 +1,24 @@
+def fibrecursive(n):
+    if n==0:
+        return 0
+    elif n==1:
+        return 1
+    else:
+         return fibrecursive(n-1) + fibrecursive(n-2)
+
+def fibIter(n):
+    prevNum = 0
+    currrentNum = 1
+    for i in range(1, n):
+        prevPrevNum = prevNum
+        prevNum = currrentNum
+        currrentNum = prevNum + prevPrevNum
+    return currrentNum
+
+
+if __name__ == "__main__":
+    n = int(input("Enter a Number:"))
+    init = time.time()
+    print ("Fibonacci Number using recursive is:  ", fibrecursive(n))
+    #print("Fibonacci Number using Iterative is:  ", fibIter(n))
+    print("Time taken: ",time.time() - init),"seconds"
